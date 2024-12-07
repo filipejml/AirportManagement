ee733ed8741a23a54f0e5253ce726f50b5a2678a
filipejml <brincandodedesign@gmail.com>
2024-12-07 18:46:40 -0300

    feat de backend

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,8 @@ Route::get('/', function () {
     return view('welcome'); // Página inicial, pode ser substituída por outra view ou lógica.
 })->name('home');
 
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/cadastro-voos', [FlightController::class, 'create'])->name('flights.create');
+Route::get('/lista-voos', [FlightController::class, 'index'])->name('flights.index');
+Route::get('/companhias-aereas', [CompanyController::class, 'index'])->name('companies.index');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard');
